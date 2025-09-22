a5b1be91892bba3f253e8cca49f4c97ee10087f3
yasmim <yasmimrussi@gmail.com>
2025-09-22 09:38:53 -0300

    arruamndo meus erros

--- meu_app/db/schema.rb
+++ meu_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_08_150149) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_123725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,9 +36,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_08_150149) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "content"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published"
   end
 
   add_foreign_key "comments", "posts"
